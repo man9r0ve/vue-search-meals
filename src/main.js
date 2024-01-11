d10ec4eaf514690fc7ea2a9e8e360dd3c26dbaf5
SH <mangrove.sh@gmail.com>
2024-01-12 00:02:07 +0900

    2024-01-12 로그인 페이지 추가

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
+
+import './style.css';
+
 import router from './router';
 import store from './store';
-import './style.css';
 import App from './App.vue';
 
 createApp(App).use(router).use(store).mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -5,27 +5,39 @@ import Home from '../views/Home.vue';
 import MealsByName from '../views/MealsByName.vue';
 import MealsByLetter from '../views/MealsByLetter.vue';
 import MealsByIngredient from '../views/MealsByIngredient.vue';
+import DefaultLayout from '../components/DefaultLayout.vue';
+import GuestLayout from '../components/GuestLayout.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home,
+    component: DefaultLayout,
+    children: [
+      {
+        path: '/',
+        name: 'home',
+        component: Home,
+      },
+      {
+        path: '/by-name/:name?',
+        name: 'byName',
+        component: MealsByName,
+      },
+      {
+        path: '/by-letter/:letter?',
+        name: 'byLetter',
+        component: MealsByLetter,
+      },
+      {
+        path: '/by-ingredient/:ingredient?',
+        name: 'byIngredient',
+        component: MealsByIngredient,
+      },
+    ],
   },
   {
-    path: '/by-name/:name?',
-    name: 'byName',
-    component: MealsByName,
-  },
-  {
-    path: '/by-letter/:letter?',
-    name: 'byLetter',
-    component: MealsByLetter,
-  },
-  {
-    path: '/by-ingredient/:ingredient?',
-    name: 'byIngredient',
-    component: MealsByIngredient,
+    path: '/guest',
+    component: GuestLayout,
   },
 ];
 
